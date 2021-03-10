0366ccae9b013e2646e8221c9e4a0f71971b4a7f
MuckT <mcgtom10@gmail.com>
2021-03-10 02:36:17 -0800

    feat: update rules page and add constants

---
+++ dist/js/rules.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const table = document.querySelector('#score table');
+console.log(table);
+
+scoreTuples.forEach(tuple => {
+  console.log(tuple);
+  let row = document.createElement('tr');
+  let td = document.createElement('td');
+  let td2 = document.createElement('td');
+  td.innerText = tuple[0];
+  td2.innerText = tuple[1];
+  row.append(td);
+  row.append(td2);
+  table.append(row);
+});
